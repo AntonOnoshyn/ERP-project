f28c101b93b46e076ec00163150574c784b0c39a
HristoE <114247445+HristoE@users.noreply.github.com>
2023-03-05 14:46:15 -0500

    configuration properties info added, Base page webelements added,

--- src/test/java/com/ERP/pages/BasePage.java
+++ src/test/java/com/ERP/pages/BasePage.java
@@ -1,7 +1,26 @@
 package com.ERP.pages;
 
+import com.ERP.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public abstract class BasePage {
-   public BasePage() {
-        PageFactory.initElements(Driver.getDriver(), this);
-    }
+   public BasePage() {PageFactory.initElements(Driver.getDriver(),this);}
+
+  @FindBy(id = "login")
+  public WebElement txt_username;
+
+  @FindBy(id= "password")
+  public WebElement txt_password;
+
+  @FindBy(xpath = "//button[@type='submit']")
+  public WebElement btn_log_in;
+
+  @FindBy(xpath = "//span[normalize-space()='Repairs']")
+  public WebElement repairsButton;
+
+
+
+
 }
