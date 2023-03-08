a8fc21ea2463b3034469040b6c9ec7c7beb13a4b
Ahmad <bilalkakr@gmail.com>
2023-03-08 14:06:58 -0500

    Different Modules Access

---
+++ src/test/java/com/ERP/pages/US8_DifferentModulesAccess.java
@@ -0,0 +1,9 @@
+package com.ERP.pages;
+
+public class US8_DifferentModulesAccess extends BasePage{
+
+    public US8_DifferentModulesAccess(){
+
+    }
+
+}
---
+++ src/test/java/com/ERP/step_definitions/US8_StepDifinitions.java
@@ -0,0 +1,27 @@
+package com.ERP.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US8_StepDifinitions {
+
+    US8_StepDifinitions stepDifinitions = new US8_StepDifinitions();
+
+    @Given("user is on the login page")
+
+    public void user_is_on_the_login_page(){
+
+    }
+    @When("user enter the valid credentials ")
+
+    public void user_enter_the_valid_credentials(){
+
+    }
+    @Then("user have access to the different modules ")
+
+    public void user_have_access_to_the_different_modules(){
+
+
+    }
+}
