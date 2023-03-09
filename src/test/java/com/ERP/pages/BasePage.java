a8a8cd7e9cf4cacf75aa1375982f99459270cb88
helay <mustafa.helay@gmail.com>
2023-03-09 10:28:42 -0500

    US66_Helay_done

--- src/test/java/com/ERP/pages/BasePage.java
+++ src/test/java/com/ERP/pages/BasePage.java
@@ -6,5 +6,17 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public abstract class BasePage {
+    public BasePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(id = "login")
+    public WebElement txt_username;
+
+    @FindBy(id = "password")
+    public WebElement txt_password;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement txt_login;
 
 }
--- src/test/java/com/ERP/pages/US66_MainModulesAccess.java
+++ src/test/java/com/ERP/pages/US66_MainModulesAccess.java
@@ -7,19 +7,12 @@ import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
 
-public class US66_MainModulesAccess {
+public class US66_MainModulesAccess extends BasePage {
     public US66_MainModulesAccess(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
-    @FindBy(id = "login")
-    public WebElement txt_username;
 
-    @FindBy(id = "password")
-    public WebElement txt_password;
-
-    @FindBy(xpath = "//button[@type='submit']")
-    public WebElement txt_login;
 
     @FindBy(xpath = "//li[@style='display: block;']")
     public List<WebElement> access22Modules;
